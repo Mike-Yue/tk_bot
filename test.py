5d2276bc87b922ebb6c9a26a42eb80948c986972
Roshan <steradian614@gmail.com>
2021-03-03 01:12:28 -0800

    Prettier logging

--- test.py
+++ test.py
@@ -29,6 +29,7 @@ for image_path in images:
 
     truth = get_killer_and_killee(image_path)
     if (len(result) == 2 and result[0] == truth[0] and result[1] == truth[1]): 
-        print("{} - PASSED".format(image_path.split('/')[-1])) 
+        print("\x1b[6;30;42m" + "{} - PASSED".format(image_path.split('/')[-1]) + "\x1b[0m") 
     else: 
-        print("{} - FAILED".format(image_path.split('/')[-1])) 
+        print("\x1b[0;30;41m" + "{} - FAILED".format(image_path.split('/')[-1]) + "\x1b[0m")
+        print("\x1b[0;30;41m" + "Raw results: \n {}".format(text_results) + "\x1b[0m")
